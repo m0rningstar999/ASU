161c64a5e048ea97179c80f7d5b76248770d024a
m0rningstar999 <56830409+m0rningstar999@users.noreply.github.com>
2019-10-21 19:31:05 +0630

    asu.py

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-pkg install python2;pip2 install bs4;pip2 install requests;pip2 install mechanize;pkg install openssh;pkg install php;python2 ASU.py
+pkg install python2 
+pip2 install bs4 
+pip2 install requests
+pip2 install mechanize
+pkg install openssh
+pkg install php
+python2 ASU.py
